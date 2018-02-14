4fddea32122251dfefe6c46b22284216b22d1e1b
Oskar Thoren <ot@oskarthoren.com>
2018-02-14 20:49:56 +0800

    mac: use first commit as start

--- mac.py
+++ mac.py
@@ -12,12 +12,16 @@ import sys
 # TODO: Clean up arg parsing / setup
 # TODO: Clean up last script funky bits
 
-git_repo = sys.argv[1]
-start = sys.argv[2]
-until = sys.argv[3]
-print "MAC", git_repo
-print "From", start, "to", until
-os.chdir(git_repo)
+# git rev-list --all | tail -n1 | xargs git log --format=%ci | xargs echo | cut -d " " -f 1
+def first_commit():
+    pipe_cmds = ["git rev-list --all", "tail -n1", "xargs git log --format=%ci", "xargs echo"]
+    procs = []
+    procs.append(subprocess.Popen(pipe_cmds[0].split(), stdout=subprocess.PIPE))
+    procs.append(subprocess.Popen(pipe_cmds[1].split(), stdin=procs[0].stdout, stdout=subprocess.PIPE))
+    procs.append(subprocess.Popen(pipe_cmds[2].split(), stdin=procs[1].stdout, stdout=subprocess.PIPE))
+    procs.append(subprocess.Popen(pipe_cmds[3].split(), stdin=procs[2].stdout, stdout=subprocess.PIPE))
+    output, err = procs[3].communicate()
+    return output[:10]
 
 def periods(start, until):
     months, res = [], []
@@ -40,9 +44,22 @@ def contributors(since, until):
     output,err = p2.communicate()
     return filter(None, output.replace('\'', '').split('\n'))
 
+# TODO: multiple repos a la
+# ./mac.py ~/git/status-react/ ~/git/status-go/ then CSV
+git_repo = sys.argv[1]
+os.chdir(git_repo)
+
+git_name = git_repo.split("/")[-2]
+#start = sys.argv[2]
+#until = sys.argv[3]
+start = first_commit()
+until = "2018-01-01"
+print "MAC", git_name, start, "-", until
+
 contributor_month = []
 for p in periods(start, until):
     contributor_month.append(sets.Set(contributors(p[0], p[1])))
 
 for m in contributor_month:
     print len(m)
+
