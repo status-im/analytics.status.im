8ad0dfd6e517b10b64c947e29eca401704262d73
Jakub Sokołowski <jakub@status.im>
2019-07-23 17:41:17 -0400

    fix bin/gh-pages.js script for deploing the page
    also add missing CNAME file
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- bin/gh-pages.js
+++ bin/gh-pages.js
@@ -1,12 +1,18 @@
 var ghpages = require('gh-pages')
+
+/* use SSH auth by default */
+let repoUrl = 'git@github.com:status-im/analytics.status.im.git'
+
+/* alternative is to use github used and API token */
+if (process.env.GH_USER != undefined) {
+  let repoUrl = ( 
+    'https://' + process.env.GH_USER + ':' + process.env.GH_TOKEN
+    + '@github.com/status-im/analytics.status.im.git'
+  )
+}
  
-ghpages.publish('dist', {
-  repo: ( 
-    'https://'
-    + process.env.GH_USER + ':'
-    + process.env.GH_TOKEN
-    + '@github.com/status-im/keycard.status.im.git'
-  ),
+ghpages.publish('public', {
+  repo: repoUrl,
   branch: 'gh-pages',
   dotfiles: true,
   silent: false
